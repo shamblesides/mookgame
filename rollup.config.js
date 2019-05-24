e510c4597d176b2ce77e7979c7bbd9072e9c011d
Nigel Nelson <nigel@prodatakey.com>
2019-05-24 13:46:18 -0600

    get rid of webpack, just use rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,30 @@ import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
 import { terser } from "rollup-plugin-terser";
 import url from "rollup-plugin-url"
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
+const prod = (process.env.NODE_ENV === 'production');
+
+const plugins = prod ? [
+  resolve(),
+  babel(),
+  json(),
+  url(),
+  terser({
+    module: true,
+    compress: {
+      arrows: false // for safari 9.3
+    }
+  }),
+] : [
+  resolve(),
+  babel(),
+  json(),
+  url(),
+  serve(),
+  livereload(),
+]
 
 export default {
   input: 'example/index.js',
@@ -11,16 +35,5 @@ export default {
     dir: 'dist',
     format: 'esm'
   },
-  plugins: [
-    resolve(),
-    babel(),
-    json(),
-    url(),
-    terser({
-      module: true,
-      compress: {
-        arrows: false // for safari 9.3
-      }
-    }),
-  ]
+  plugins
 };
